b1d4a203d2f5e03fffc5fe5447465d68bd274cd6
lhoff98 <lhoff986@myfcc.frederick.edu>
2026-09-07 09:40:33 -0400

    Added array, first line, and loop to print story.

---
+++ src/PassTheStory.java
@@ -0,0 +1,19 @@
+public class PassTheStory
+{
+    public static void main (String[] args)
+    {
+        int MAXLINES = 20;
+        String[] story = new String[MAXLINES];
+        story[0] = "There once was a silly little goose named Bruce, " +
+                "and he once dropped his flute, and his friends sang. 'what cha lose goose?'" +
+                "and then ";
+        
+        for(int i = 0; i < MAXLINES; i++)
+        {
+            if(story[i] != null) //skips null lines
+            {
+                System.out.println(story[i]);
+            }
+        }
+    }
+}
