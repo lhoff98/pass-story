35766b3abb14b4f7f3eeaf98ff6747de3455dbbe
jsouthard-fcc <jsouthard@frederick.edu>
2026-09-09 15:16:13 -0400

    Large dog appears

--- src/PassTheStory.java
+++ src/PassTheStory.java
@@ -7,6 +7,7 @@ public class PassTheStory
         story[0] = "There once was a silly little goose named Bruce, " +
                 "and he once dropped his flute, and his friends sang. 'what cha lose goose?'" +
                 "and then ";
+        story[1] = "a large dog appeared, wagging its tail, with a bone in its mouth; and then...";
         
         for(int i = 0; i < MAXLINES; i++)
         {
